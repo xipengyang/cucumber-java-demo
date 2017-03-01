0a0b35f017ad7b0391dcdaff3a8dc8f3d82ca734
David <xipengyang@gmail.com>
2017-03-01 16:00:06 +1300

    first commit contains: a basic spring boot project

---
+++ src/main/java/com/github/xipengyang/demo/SpringbootCucumberApplication.java
@@ -0,0 +1,12 @@
+package com.github.xipengyang.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCucumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCucumberApplication.class, args);
+    }
+}
